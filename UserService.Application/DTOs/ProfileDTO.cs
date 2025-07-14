a4ffc9a0db573ba6891c18323c0e62b733fa7fef
Dhruvit <dhruvit.patel@xduce.com>
2025-07-14 23:33:06 +0530

    Add project files.

---
+++ UserService.Application/DTOs/ProfileDTO.cs
@@ -0,0 +1,10 @@
+﻿public class ProfileDTO
+{
+    public Guid UserId { get; set; }
+    public string? FullName { get; set; } = null!;
+    public string? PhoneNumber { get; set; } = null!;
+    public string? Email { get; set; } = null!;
+    public string? UserName { get; set; } = null!;
+    public DateTime? LastLoginAt { get; set; }
+    public string? ProfilePhotoUrl { get; set; }
+}
