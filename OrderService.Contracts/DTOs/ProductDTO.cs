23eb1520d4c335ca2167d78597e06b1ed9d1490a
Dhruvit <dhruvit.patel@xduce.com>
2025-07-30 12:01:06 +0530

    Inter communication service using HTTP clinet factory added. Order-> User, Order->Product

---
+++ OrderService.Contracts/DTOs/ProductDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace OrderService.Contracts.DTOs
+{
+    public class ProductDTO
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public decimal Discount { get; set; }
+    }
+}
---
+++ OrderService.Contracts/ExternalServices/IProductServiceClient.cs
@@ -0,0 +1,11 @@
+﻿using OrderService.Contracts.DTOs;
+namespace OrderService.Contracts.ExternalServices
+{
+    public interface IProductServiceClient
+    {
+        Task<List<ProductDTO>?> GetProductsByIdsAsync(List<Guid> productIds, string accessToken);
+        Task<List<ProductStockVerificationResponseDTO>?> CheckProductsAvailabilityAsync(List<ProductStockVerificationRequestDTO> requestedItems, string accessToken);
+        Task<bool> IncreaseStockBulkAsync(IEnumerable<UpdateStockRequestDTO> stockUpdates, string accessToken);
+        Task<bool> DecreaseStockBulkAsync(IEnumerable<UpdateStockRequestDTO> stockUpdates, string accessToken);
+    }
+}
